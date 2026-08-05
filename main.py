4b2c7535b8e6eecc8f1558065fd843169fef82cd
Aryan Antad <aryanantad@gmail.com>
2026-08-06 00:29:14 +0530

    Added root endpoint

--- main.py
+++ main.py
@@ -421,3 +421,21 @@ async def search_bundle(
         tfidf_recommendations=tfidf_items,
         genre_recommendations=genre_recs,
     )
+
+@app.get("/")
+def root():
+    return {
+        "message": "Movie Recommendation API is running!",
+        "docs": "/docs"
+    }
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {
+        "message": "Movie Recommendation API is running!",
+        "docs": "/docs"
+    }
